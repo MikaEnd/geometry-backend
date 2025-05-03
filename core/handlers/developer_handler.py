895970878fb80afa050c81d9f6ce8e8f3013e863
MikaEnd <yarparty@gmail.com>
2025-05-03 17:23:59 +0300

    Automatic sync 2025-05-03 17:23:59

--- core/handlers/developer_handler.py
+++ core/handlers/developer_handler.py
@@ -4,6 +4,4 @@ from core.skills.execute_with_llm import ExecuteWithLLMSkill
 class DeveloperHandler(BaseHandler):
     def __init__(self):
         super().__init__()
-        self.skills = [
-            ExecuteWithLLMSkill()
-        ]
+        self.skills = [ExecuteWithLLMSkill()]
