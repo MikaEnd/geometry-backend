37e8211613008bdd921a29512fee2e87d5768acb
MikaEnd <yarparty@gmail.com>
2025-05-04 15:45:55 +0300

    Automatic sync 2025-05-04 15:45:55

--- core/base_handler.py
+++ core/base_handler.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 
 class BaseHandler(ABC):
     @abstractmethod
+    def can_handle(self, message: str) -> bool:
+        pass
+
+    @abstractmethod
     async def handle(self, message: str, user_id: str) -> str:
-        """Обрабатывает входящее сообщение от пользователя."""
         pass
