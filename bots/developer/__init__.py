beaddd91fd31895e4cb800c7f5b5c92446bd9532
MikaEnd <yarparty@gmail.com>
2025-05-08 01:24:36 +0300

    Automatic sync 2025-05-08 01:24:36

--- bots/developer/__init__.py
+++ bots/developer/__init__.py
@@ -1,8 +1,8 @@
-from core.interfaces import Handler
+from core.interfaces import BotHandler
 from core.skills.generate_code import GenerateCodeSkill
 from core.skills.write_file import WriteFileSkill
 
-class DeveloperHandler(Handler):
+class DeveloperHandler(BotHandler):
     def __init__(self):
         self.skills = [
             GenerateCodeSkill(),
